52063eb70f2800817bcbc0c214289584fd0e79c8
fharaujo <faraujodeveloper@hotmail.com>
2016-10-26 00:51:19 -0200

    3fileEx

--- Soma de Dois Numeros.py
+++ Soma de Dois Numeros.py
@@ -2,4 +2,4 @@ numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo número: "))
 
 resultado = numero1 + numero2
-print("A soma de %i + %i = %i"%(numero1, numero2, resultado))
+print("Somando %i + %i = %i"%(numero1, numero2, resultado))
